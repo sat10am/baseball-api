60ee99ed4a2e8dca0d02088f1d18b239a08e0f6c
DoonDoony <cream.doondoon@gmail.com>
2019-10-05 13:02:16 +0900

    feat: create judge function

--- doon/baseball/api/views.py
+++ doon/baseball/api/views.py
@@ -2,6 +2,33 @@ from django.http import JsonResponse
 import attr
 
 # Create your views here.
+def judge(number, real_number):
+    strikes = 0
+    balls = 0
+    out = 0
+
+    for i, real_i in zip(number, real_number):
+        if i == real_i:
+            strikes += 1
+            continue
+        if i in real_number:
+            balls += 1
+            continue
+
+    # Both strikes and balls are zero
+    if not (strikes or balls):
+        return "1O"
+
+    # Neither balls and out
+    if strikes and not balls:
+        return "4S"
+    # Neither strikes and out
+
+    if not strikes:
+        return "4B"
+
+    # Either strikes and balls are not zero
+    return f"{strikes}S {balls}B"
 
 
 @attr.s
@@ -26,5 +53,10 @@ def get_response(
 
 def get(request):
     tried_number = request.GET.get("number")
-    response = get_response("1B 3S", "1234", 10)
+    real_number = "1234"
+    judgement = judge(tried_number, real_number)
+    if judgement == "4S":
+        response = get_response(judgement, tried_number, 10, True)
+        return JsonResponse(attr.asdict(response), status=201)
+    response = get_response(judgement, tried_number, 10)
     return JsonResponse(attr.asdict(response))
