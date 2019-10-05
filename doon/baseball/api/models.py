bf8484e5724023dfd1011f49d842fa570ddd2704
DoonDoony <cream.doondoon@gmail.com>
2019-10-05 12:37:01 +0900

    feat: create app; make model

--- doon/baseball/api/models.py
+++ doon/baseball/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class RequestLog(models.Model):
+
+    session = models.TextField()
+    number = models.CharField(max_length=4)
+    real_number = models.CharField(max_length=4)
+    created_at = models.DateTimeField(auto_now_add=True)
