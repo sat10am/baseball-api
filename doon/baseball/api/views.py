3da03cfea761c749769b5d280241758cd081d40e
DoonDoony <cream.doondoon@gmail.com>
2019-10-05 12:53:44 +0900

    feat: create response type

--- doon/baseball/api/views.py
+++ doon/baseball/api/views.py
@@ -1,8 +1,30 @@
 from django.http import JsonResponse
+import attr
 
 # Create your views here.
 
 
+@attr.s
+class BaseballAPIResponse:
+
+    JUDGEMENT: str = attr.ib()
+    TRIED_NUMBER: str = attr.ib()
+    CORRECT: bool = attr.ib()
+    TRY_COUNT: int = attr.ib()
+
+
+def get_response(
+    judgement: str, tried_number: str, try_count: int, correct: bool = False
+):
+    return BaseballAPIResponse(
+        JUDGEMENT=judgement,
+        TRIED_NUMBER=tried_number,
+        TRY_COUNT=try_count,
+        CORRECT=correct,
+    )
+
+
 def get(request):
-    print(request)
-    return JsonResponse({"JUDGEMENT": "4B"})
+    tried_number = request.GET.get("number")
+    response = get_response("1B 3S", "1234", 10)
+    return JsonResponse(attr.asdict(response))
